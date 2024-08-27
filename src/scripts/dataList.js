92d0da6e790d45166b4adc5b8266e76df03e56d5
hsc996 <han.scaife@gmail.com>
2024-08-27 19:34:13 +1000

    feat: html and js skeleton

---
+++ src/scripts/dataList.js
@@ -0,0 +1,16 @@
+let dataArray = [
+    "warthog",
+    "interceptor",
+    "herbie",
+    "lightning mcqueen",
+    "batmobile",
+    "bat tumbler",
+    "delorean",
+    "eleanor",
+    "magic school bus",
+    "toyota corolla"
+]
+
+dataArray.forEach((plants) => {
+    console.log
+})
